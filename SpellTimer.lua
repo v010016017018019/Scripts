beee8a3f7273547d843f8980ac837f3b41abda2d
v010016017018019 <v010016017018019@gmail.com>
2016-02-13 02:03:43 +0900

    version
    0.2

--- SpellTimer.lua
+++ SpellTimer.lua
@@ -12,27 +12,38 @@ local ts = t
 local tm = 0
 local ts = math.floor(ts)
 
-if ed.currentCd > 0 and os.clock() - edlastMessage > 30 then
+if ed.currentCd > 0 and os.clock() - edlastMessage > 60 then
 ts = ts +ed.currentCd
 local ts = math.floor(ts)
 while ts >= 60 do
   tm = tm + 1
   ts = ts - 60
   end
+  edlastMessage = os.clock()	
+	if ed.name ~= summonerflash then
+PrintChat("<font color=\"#A27AFE\"><b>"..enemy.charName.."<font color=\"#FF4848\"> FALSH ON:</font> </b><font color=\"#FFFFFF\">" .. tm .." : "..ts)
+elseif ed.name ~= summonerdot then
+PrintChat("<font color=\"#A27AFE\"><b>"..enemy.charName.."<font color=\"#FF4848\"> Ignite ON:</font> </b><font color=\"#FFFFFF\">" .. tm .." : "..ts)
+else
 PrintChat("<font color=\"#A27AFE\"><b>"..enemy.charName.."<font color=\"#FF4848\"> D ON:</font> </b><font color=\"#FFFFFF\">" .. tm .." : "..ts)
-edlastMessage = os.clock()	
 end
 
-if ef.currentCd > 0 and os.clock() - eflastMessage > 30 then
+if ef.currentCd > 0 and os.clock() - eflastMessage > 60 then
 ts = ts +ef.currentCd
 local ts = math.floor(ts)
 while ts >= 60 do
   tm = tm + 1
   ts = ts - 60
   end
-PrintChat("<font color=\"#A27AFE\"><b>"..enemy.charName.."<font color=\"#FF4848\"> F ON:</font> </b><font color=\"#FFFFFF\">" .. tm .." : "..ts)
 eflastMessage = os.clock()	
-
+	if ef.name ~= summonerflash then
+PrintChat("<font color=\"#A27AFE\"><b>"..enemy.charName.."<font color=\"#FF4848\"> FALSH ON:</font> </b><font color=\"#FFFFFF\">" .. tm .." : "..ts)
+elseif ef.name ~= summonerdot then
+PrintChat("<font color=\"#A27AFE\"><b>"..enemy.charName.."<font color=\"#FF4848\"> Ignite ON:</font> </b><font color=\"#FFFFFF\">" .. tm .." : "..ts)
+else
+PrintChat("<font color=\"#A27AFE\"><b>"..enemy.charName.."<font color=\"#FF4848\"> F ON:</font> </b><font color=\"#FFFFFF\">" .. tm .." : "..ts)
+end
+end
 end
 end
 end
